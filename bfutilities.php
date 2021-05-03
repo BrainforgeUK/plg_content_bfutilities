aaeb88c913b61ca64ab0b36b77f802f6b08a8ea3
Brainforge.UK <brainforge.uk@btinternet.com>
2021-05-03 21:58:44 +0100

    Tries to get any missing plugin languages.

--- bfutilities.php
+++ bfutilities.php
@@ -2,16 +2,17 @@
 /**
  * @package Plugin with various utilities, such as current date display and language translation
  * @version 1.0.0
- * @copyright Copyright (C) 2020 Jonathan Brain - brainforge. All rights reserved.
+ * @copyright Copyright (C) 2020-2021 Jonathan Brain - brainforge. All rights reserved.
  * @license GNU General Public License version 2 or later; see LICENSE.txt, see LICENSE.php
  * @author https://www.brainforge.co.uk
  */
 
-// No direct access
-defined('_JEXEC') or die;
-
+use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Plugin\CMSPlugin;
+
+// No direct access
+defined('_JEXEC') or die;
 
 class plgContentbfutilities extends CMSPlugin
 {
@@ -40,6 +41,21 @@ class plgContentbfutilities extends CMSPlugin
 						return date($matches[2]);
 					case 'text':
 						$args = explode(',', $matches[2]);
+
+						switch (substr($args[0], 0, 4))
+						{
+							case 'PLG_':
+								$extension = explode('_', strtolower($args[0]), 4);
+								if (count($extension) == 4)
+								{
+									unset($extension[3]);
+									$lang = Factory::getApplication()->getLanguage();
+									$lang->load(implode('_', $extension), JPATH_ADMINISTRATOR);
+									$lang->load(implode('_', $extension), JPATH_PLUGINS);
+								}
+								break;
+						}
+
 						if (count($args) == 1)
 						{
 							return $this->prepareContent(Text::_($matches[2]));
